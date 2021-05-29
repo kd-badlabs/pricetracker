d1343bc410e8aa5039c778a3040a91ff3b9f328b
kd <badlabs.kd@gmail.com>
2021-05-30 00:56:07 +0530

    updatedb

--- project/settings.py
+++ project/settings.py
@@ -90,23 +90,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    ‘default’: {
-        ‘ENGINE’: ‘django.db.backends.postgresql_psycopg2’,
-        ‘NAME’: env('DB_NAME'),
-        ‘USER’: env('DB_USER'),
-        ‘PASSWORD’: env('DB_PASSWORD'),
-        'HOST':env('DB_HOST'),
-        'PORT':env('DB_PORT'),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     ‘default’: {
+#         ‘ENGINE’: ‘django.db.backends.postgresql_psycopg2’,
+#         ‘NAME’: env('DB_NAME'),
+#         ‘USER’: env('DB_USER'),
+#         ‘PASSWORD’: env('DB_PASSWORD'),
+#         'HOST':env('DB_HOST'),
+#         'PORT':env('DB_PORT'),
+#         }
+# }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
