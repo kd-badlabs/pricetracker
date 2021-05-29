2e7336b7aa0f90d71a06f7015ae509668a7e22da
kd <badlabs.kd@gmail.com>
2021-05-29 19:03:50 +0530

    deploy-channel

--- project/asgi.py
+++ project/asgi.py
@@ -1,21 +1,46 @@
 
+# import os
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# from django.urls import path
+# from stock.consumers import *
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+ 
+# application = get_asgi_application()    
+
+
+# ws_patterns =[ 
+#     path('ws/new/',Stock_Async.as_asgi()) ,  
+# ]
+
+
+# application = ProtocolTypeRouter({
+#     "websocket": URLRouter(ws_patterns),
+# })
+
+
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_default_application
 from django.urls import path
 from stock.consumers import *
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-application = get_asgi_application()    
+django.setup() 
+application = get_default_application()    
 
 
 ws_patterns =[ 
     path('ws/new/',Stock_Async.as_asgi()) ,  
 ]
 
-
 application = ProtocolTypeRouter({
     "websocket": URLRouter(ws_patterns),
 })
 
+ 
+
+ 
