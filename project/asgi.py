514ae64a1c14640dad3c55f529f9ad54e8622bed
kd <badlabs.kd@gmail.com>
2021-05-29 23:15:32 +0530

    2

--- project/asgi.py
+++ project/asgi.py
@@ -2,15 +2,15 @@
 
 import os
 import django
-# from channels.routing import get_default_application
+from channels.routing import get_default_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 from stock.consumers import *
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
-# application = get_default_application()
-application = get_asgi_application()    
+application = get_default_application()
+# application = get_asgi_application()    
 ws_patterns =[ 
     path('ws/new/',Stock_Async.as_asgi()) ,  
 ]
