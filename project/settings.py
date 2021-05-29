b0f140900f7407281a59d29fe0a559d574bb12ab
kd <badlabs.kd@gmail.com>
2021-05-29 20:08:43 +0530

    deploy_channel

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com","127.0.0.1:8000",]
 
