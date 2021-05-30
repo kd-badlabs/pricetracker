fcf41406562a4b49ec8ac6bd2a5364fbabfaf947
kd <badlabs.kd@gmail.com>
2021-05-30 22:36:53 +0530

    bd_2

--- searchBar/views.py
+++ searchBar/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.db.models import Q
 from .models import Company_symbol
 from django.http import JsonResponse
+from django.conf import settings
 import pandas as pd
 import numpy as np
+import os
 
 # Create your views here.
 def uploadData(request):
-    df = pd.read_csv("searchBar/nasdaq_screener.csv")
+    df = pd.read_csv(os.path.join(settings.ROOT_PATH, 'nasdaq_screener.csv'))
     df.fillna("NA",inplace=True)
     records = df.to_dict('records')
     for record in records: 
@@ -24,6 +26,7 @@ def uploadData(request):
 
 def search_result(request,data): 
     results=Company_symbol.objects.filter(Q(symbol=data)).values_list()
+    print(results)
     return JsonResponse({'status' : 200,"results":list(results)})
   
     
