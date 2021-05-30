de20f85ea6fffa1bcbf02d2410355a0881a1c5e0
kd <badlabs.kd@gmail.com>
2021-05-31 00:16:51 +0530

    db_csv_1

--- searchBar/urls.py
+++ searchBar/urls.py
@@ -4,4 +4,5 @@ from .views import search_result,uploadData
 urlpatterns = [
     path('searchresult/<data>/',search_result),
     path('uploadData/',uploadData),  
+    path('uploadData_1/',uploadData_1),  
 ]
